e820176940a212d9d023f0a6b90ff7c01a7af6c6
James Pain <james@thinkbit.uk>
2024-12-01 18:15:41 +0000

    Update day01.py

--- day01.py
+++ day01.py
@@ -4,17 +4,17 @@ import os
 inputFileName = os.path.basename(__file__).replace('.py', '-input.txt')
 
 def part1():
-    thefirstofthetwonumbers_list = []
-    thesecondofthetwonumbers_list = []
+    one_list = []
+    two_list = []
     with open(inputFileName) as fileInput:
         for line in fileInput:
-            thefirstofthetwonumbers, thesecondofthetwonumbers = map(int, line.split())
-            thefirstofthetwonumbers_list.append(thefirstofthetwonumbers)
-            thesecondofthetwonumbers_list.append(thesecondofthetwonumbers)
-    thefirstofthetwonumbers_list.sort()
-    thesecondofthetwonumbers_list.sort()
+            one, two = map(int, line.split())
+            one_list.append(one)
+            two_list.append(two)
+    one_list.sort()
+    two_list.sort()
     
-    distances = [abs(thefirstofthetwonumbers - thesecondofthetwonumbers) for thefirstofthetwonumbers, thesecondofthetwonumbers in zip(thefirstofthetwonumbers_list, thesecondofthetwonumbers_list)]
+    distances = [abs(one - two) for one, two in zip(one_list, two_list)]
     total_distance = sum(distances)
     for distance in enumerate(distances):
         print(distance)
@@ -22,18 +22,18 @@ def part1():
 
 
 def part2():
-    thefirstofthetwonumbers_list = []
-    thesecondofthetwonumbers_list = []
+    one_list = []
+    two_list = []
     with open(inputFileName) as fileInput:
         for line in fileInput:
-            thefirstofthetwonumbers, thesecondofthetwonumbers = map(int, line.split())
-            thefirstofthetwonumbers_list.append(thefirstofthetwonumbers)
-            thesecondofthetwonumbers_list.append(thesecondofthetwonumbers)
+            one, two = map(int, line.split())
+            one_list.append(one)
+            two_list.append(two)
     
     similarity_score = 0
-    for thefirstofthetwonumbers in thefirstofthetwonumbers_list:
-        count_in_thesecondofthetwonumbers = thesecondofthetwonumbers_list.count(thefirstofthetwonumbers)
-        similarity_score += thefirstofthetwonumbers * count_in_thesecondofthetwonumbers
+    for one in one_list:
+        count_in_two = two_list.count(one)
+        similarity_score += one * count_in_two
     
     print('Total:', similarity_score)
 
