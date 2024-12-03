58a9103382b0aa8f1aa53bc0751712992af285d2
jpain-cat <james.pain@checkatrade.com>
2024-12-03 19:36:04 +0000

    Day02 Finished

--- day02.py
+++ day02.py
@@ -26,31 +26,31 @@ def part1():
                 safe_reports += 1
         print(safe_reports)
 
+def is_safe(report):
+    is_increasing = None
+    for i in range(len(report) - 1):
+        diff = report[i + 1] - report[i]
+        if diff > 3 or diff == 0 or diff < -3:
+            return False
+        if is_increasing is None:
+            is_increasing = diff > 0
+        elif (is_increasing and diff < 0) or (not is_increasing and diff > 0):
+            return False
+    return True
+
 def part2():
     safe_reports = 0
     with open(inputFileName) as fileInput:
         for line in fileInput.readlines():
             current_list = list(map(int, line.split()))
-            print(current_list)
-            is_increasing = None
-            valid = True
-            issues_in_line = 0
-            for i in range(len(current_list) - 1):
-                diff = current_list[i + 1] - current_list[i]
-                if is_increasing is None and diff != 0:
-                    is_increasing = diff > 0
-                if diff > 3 or diff == 0 or diff < -3:
-                    print('Diff > 3 or 0 or < -3')
-                    issues_in_line = issues_in_line + 1
-                elif (is_increasing and diff < 0) or (not is_increasing and diff > 0):
-                    issues_in_line = issues_in_line + 1
-                if issues_in_line > 1:
-                    print('not safe')
-                    valid = False
-                    break
-            if valid:
-                print('safe')
+            if is_safe(current_list):
                 safe_reports += 1
+            else:
+                for i in range(len(current_list)):
+                    modified_list = current_list[:i] + current_list[i+1:]
+                    if is_safe(modified_list):
+                        safe_reports += 1
+                        break
         print(safe_reports)
 
 if __name__ == "__main__":
